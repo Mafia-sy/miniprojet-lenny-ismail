ba635950f9240d78d990a2bcacabf03ce977ea7c
ISMAIL <132509001+Mafia-sy@users.noreply.github.com>
2025-10-10 09:53:31 +0200

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,8 +0,0 @@
-// hello.cpp
-#include "hello.h"
-#include <string>
-std::string hello(const std::string& name) {
-	std::string strRet = "Hello, " + name + "!";
-	std::cout << strRet << std::endl;	
-      	return strRet;
-}
